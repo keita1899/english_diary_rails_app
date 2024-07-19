e8d377acce13e3ee877646396aa0af749457f1ae
keita1899 <saboten18k@gmail.com>
2024-07-19 23:27:30 +0900

    rspec及びテスト関連のセットアップ

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -33,5 +33,16 @@ module Myapp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.assets false
+      g.helper     false
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        request_specs: false
+    end
   end
 end
