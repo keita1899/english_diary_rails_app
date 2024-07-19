5ab9659ed9919b6c21fadccddaa9dbfebb631037
keita1899 <saboten18k@gmail.com>
2024-07-20 00:49:05 +0900

    デバッグ用のgemをインストール

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -45,7 +45,7 @@ module Myapp
                        request_specs: false
     end
 
-    config.time_zone = "Tokyo"
+    config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
   end
 end
