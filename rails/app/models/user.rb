333ae2482d39bac725fad2701eb64a9f894ea064
keita1899 <saboten18k@gmail.com>
2024-07-22 22:21:04 +0900

    nameのバリデーションの条件を削除、パスワードのフォーマットのエラーメッセージを修正

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     validates :password_confirmation
   end
 
-  validates :name, length: { maximum: 50 }, if: -> { name.present? }
+  validates :name, length: { maximum: 50 }
   validates :email, length: { maximum: 255 }, uniqueness: true, format: { with: VALID_EMAIL_REGEX }, if: -> { email.present? }
 
   has_secure_password
@@ -23,7 +23,7 @@ class User < ApplicationRecord
       return if password.blank?
 
       unless password =~ /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/
-        errors.add(:password, 'は半角英数字で、英字と数字の両方を含む必要があります')
+        errors.add(:password, 'は半角英数字で、少なくとも1つの英字と1つの数字を含む必要があります')
       end
     end
 end
