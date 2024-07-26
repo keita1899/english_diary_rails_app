dc4c9d698859cbb57435ac036e4c95b43c271cef
keita1899 <saboten18k@gmail.com>
2024-07-26 12:27:19 +0900

    ログイン機能作成

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'top#index'
   get  '/signup', to: 'users#new'
   resources :users
+  resources :sessions, only: %i[new create]
+  resources :diaries, only: %i[index]
 end
