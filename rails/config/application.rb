d20a718d9aa66ad1666332cec6df1a5d94f23fb6
keita1899 <saboten18k@gmail.com>
2024-10-22 18:40:19 +0900

    プロフィール編集機能作成

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -49,5 +49,7 @@ module Myapp
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
+    config.active_storage.variant_processor = :mini_magick
   end
 end
